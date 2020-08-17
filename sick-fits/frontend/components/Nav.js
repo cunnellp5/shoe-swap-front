94800bd3d5b1dba2356f70074044e103570e4609
cunnellp5 <philip.cunnell@colorado.edu>
2020-08-17 07:12:59 -0600

    front end react skate swap

---
+++ sick-fits/frontend/components/Nav.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const nav = () => (
+    <div>
+        <Link href="/sell"><a>sell</a></Link>
+        <Link href="/"><a>home</a></Link>
+    </div>
+)
+
+export default nav;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,7 @@
+const Sell = props => (
+    <div>
+        <p>sello</p>
+    </div>
+)
+
+export default Sell
